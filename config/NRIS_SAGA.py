0c4fe768a1a2208f6563ad630ad40ab9adc5c5c7
Richard Top <richard.topouchian@uib.no>
2024-04-10 07:34:08 +0000

    modified qcpu in config/NRIS_SAGA.py added -mem

--- config/NRIS_SAGA.py
+++ config/NRIS_SAGA.py
@@ -47,7 +47,7 @@ site_configuration = {
                     ],
                     'launcher': 'mpirun',
                     # Use --export=None to avoid that login environment is passed down to submitted jobs
-                    'access': ['-p normal', '-A nn9992k', -mem='16', '--export=None'],
+                    'access': ['-p normal', '-A nn9992k', -mem '16', '--export=None'],
                     'environs': ['default'],
                     'max_jobs': 120,
                     'features': [
