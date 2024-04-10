ebd30264c21ac2ff0bad7d218a66602933ea054e
Richard Top <richard.topouchian@uib.no>
2024-04-10 07:38:53 +0000

    modified qcpu in config/NRIS_SAGA.py disabled remote detection

--- config/NRIS_SAGA.py
+++ config/NRIS_SAGA.py
@@ -107,7 +107,7 @@ site_configuration = {
         {
             # Enable automatic detection of CPU architecture for each partition
             # See https://reframe-hpc.readthedocs.io/en/stable/configure.html#auto-detecting-processor-information
-            'remote_detect': True,
+            'remote_detect': False,
             **common_general_config(reframe_prefix)
         }
     ],
